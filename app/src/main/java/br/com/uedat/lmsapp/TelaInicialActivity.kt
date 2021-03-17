9010ff1430c3aff861aac130713729643b9bf42b
uedatt <victor.sanches@aluno.faculdadeimpacta.com.br>
2021-03-17 02:50:01 -0300

    commit novos temas

--- app/src/main/java/br/com/uedat/lmsapp/TelaInicialActivity.kt
+++ app/src/main/java/br/com/uedat/lmsapp/TelaInicialActivity.kt
@@ -32,12 +32,12 @@ class TelaInicialActivity : DebugActivity() {
     override fun onOptionsItemSelected(item: MenuItem): Boolean{
         val id = item.itemId
         if (id == R.id.action_buscar){
-            Toast.makeText(this, "clicou no buscar", Toast.LENGTH_LONG).show()
+            Toast.makeText(this, "clicou em buscar", Toast.LENGTH_LONG).show()
         }else if (id == R.id.action_atualizar){
-            Toast.makeText(this, "clicou no atualizar", Toast.LENGTH_LONG).show()
+            Toast.makeText(this, "clicou em atualizar", Toast.LENGTH_LONG).show()
 
         }else if (id == R.id.action_config){
-            Toast.makeText(this, "clicou no config", Toast.LENGTH_LONG).show()
+            Toast.makeText(this, "clicou em configurações", Toast.LENGTH_LONG).show()
 
         }else if(id == android.R.id.home){
             finish()

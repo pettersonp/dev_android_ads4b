5c6693351d181c76e7a10cc3bd685ff184169bcd
uedatt <victor.sanches@aluno.faculdadeimpacta.com.br>
2021-04-13 22:33:31 -0300

    Arquivos Ac03

--- app/src/main/java/br/com/uedat/lmsapp/TelaInicialActivity.kt
+++ app/src/main/java/br/com/uedat/lmsapp/TelaInicialActivity.kt
@@ -9,17 +9,20 @@ import android.view.View
 import android.view.ViewAnimationUtils
 import android.widget.TextView
 import android.widget.Toast
+import androidx.appcompat.app.ActionBarDrawerToggle
 import androidx.core.view.isVisible
+import com.google.android.material.navigation.NavigationView
 import kotlinx.android.synthetic.*
 import kotlinx.android.synthetic.main.activity_tela_inicial.*
 import kotlinx.android.synthetic.main.login.*
+import kotlinx.android.synthetic.main.toolbar.*
 import org.w3c.dom.Text
 import kotlin.concurrent.thread
 import kotlin.concurrent.timer
 import java.util.Timer
 import kotlin.concurrent.schedule
 
-class TelaInicialActivity : DebugActivity() {
+class TelaInicialActivity : DebugActivity(), NavigationView.OnNavigationItemSelectedListener {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_tela_inicial)
@@ -27,16 +30,30 @@ class TelaInicialActivity : DebugActivity() {
         var params = intent.extras
         var nome = params?.getString("nome")
         Toast.makeText(this, "nome do usuario: $nome", Toast.LENGTH_LONG).show()
+        setSupportActionBar(toolbar)
+
+
 
         supportActionBar?.title = "Musicas"
         supportActionBar?.setDisplayHomeAsUpEnabled(true)
+
+
+
+
+
+
         butMusics.setOnClickListener {
 
             val intent = Intent(this, SegundaActivity::class.java)
             var name = "Playlists"
             intent.putExtra("name","$name")
             startActivity(intent)
+            ConfigMenuLateral()
         }
+
+
+
+
         butArtists.setOnClickListener {
 
             val intent = Intent(this, SegundaActivity::class.java)
@@ -57,7 +74,26 @@ class TelaInicialActivity : DebugActivity() {
 
 
 
+
     }
+    private fun ConfigMenuLateral(){
+        var toogle = ActionBarDrawerToggle(this, layout_menu_lateral,toolbar, R.string.nav_open, R.string.nav_close )
+        layout_menu_lateral.addDrawerListener(toogle)
+        toogle.syncState()
+        naveg_menu_lateral.setNavigationItemSelectedListener(this)
+
+
+    }
+
+
+
+    override fun onNavigationItemSelected(item: MenuItem): Boolean {
+        return true
+
+    }
+
+
+
     override fun onCreateOptionsMenu(menu: Menu?): Boolean {
         menuInflater.inflate(R.menu.menu_main, menu)
         return true
